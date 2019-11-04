bdda681bb4fc20f5f9babce796a441bf7bf7c83d
Colin Turing <colin.koga@gmail.com>
2019-11-03 18:05:07 -0700

    Add class layout for Manager and makeBooking method for User

--- src/User.js
+++ src/User.js
@@ -8,7 +8,19 @@ class User {
     return this.name
   }
 
-
+  makeBooking(id, userID, date, roomNumber) {
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: Date.now(),
+        userID: this.id,
+        date: 
+      })
+    })
+  }
 }
 
 export default User;
